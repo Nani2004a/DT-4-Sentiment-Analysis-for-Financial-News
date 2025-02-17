87296d22e0466680990951d6405265a85c89959d
Nikhila1619 <nikhila9381@gmail.com>
2025-02-17 10:02:55 +0530

    hii

--- app.py
+++ app.py
@@ -1,7 +1,35 @@
-from flask import Flask,render_template
-app=Flask(__name__)
-@app.route("/")
+from flask import Flask, render_template, request, jsonify
+from test import TextToNum
+import pickle
+
+app = Flask(__name__)
+
+@app.route('/')
 def home():
-    return render_template("index.html")
-if __name__=="__main__":
-    app.run()
+    return render_template('index.html')
+
+@app.route('/predict', methods=["GET", "POST"])
+def predict():
+    if request.method == "POST":
+        msg = request.form.get("message")
+        print(msg)
+        ob = TextToNum(msg)
+        ob.cleaner()
+        ob.token()
+        ob.removeStop()
+        st = ob.stemme()
+        with open("vectorizer.pickle", "rb") as vcfile:
+            vc = pickle.load(vcfile)
+        stvc = " ".join(st)
+        data = vc.transform([stvc])
+        print(data)
+        with open("model.pickle", "rb") as mbfile:
+            model = pickle.load(mbfile)
+        pred = model.predict(data)
+        return jsonify({"result": str(pred[0])})
+    
+    # **Fix: Add 'return' here**
+    return render_template("predict.html")  # Ensure response for GET request
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5050)
